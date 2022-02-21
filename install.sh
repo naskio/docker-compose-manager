8c2d5dd7600ed69a9a5cc060048349963d24d8e7
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-21 16:37:45 +0100

    adding install.sh script

--- install.sh
+++ install.sh
@@ -1,4 +1,67 @@
 #!/bin/bash
 
-echo "Installing Docker Compose Manager ..."
-echo "Installation finished."
+##########################################################
+########## Docker Compose Manager Installation  ##########
+##########################################################
+
+
+########## Parsing args ##########
+VERSION="$1"
+if [ -z "$VERSION" ]; then
+    VERSION="arrow-keys"
+fi
+########## Parsing args ##########
+
+
+########## Getting the platform ##########
+PLATFORM=""
+UNAME_OUT="$(uname -s)"
+case "$UNAME_OUT" in
+    Linux*)     PLATFORM=Linux;;
+    Darwin*)    PLATFORM=Mac;;
+    CYGWIN*)    PLATFORM=Cygwin;;
+    MINGW*)     PLATFORM=MinGw;;
+    *)          PLATFORM="UNKNOWN:${UNAME_OUT}"
+esac
+########## Getting the platform ##########
+
+
+########## Installing dependencies ##########
+case $VERSION in
+  "dialog")
+    echo "Installing Docker Compose Manager - $VERSION ..."
+    echo "Installing dependencies ..."
+    if [ $PLATFORM=="Linux" ]; then
+      sudo apt-get update -y
+      sudo apt-get install -y dialog
+    fi
+    if [ $PLATFORM=="Mac" ]; then
+      brew update
+      brew install dialog
+    fi
+    ;;
+  "arrow-keys")
+    echo "Installing Docker Compose Manager - $VERSION ..."
+    ;;
+  "ps3")
+    echo "Installing Docker Compose Manager - $VERSION ..."
+    ;;
+  *)
+    echo "- Invalid option '$VERSION'. Should be: 'dialog', 'ps3' or 'arrow-keys'."
+    exit 1
+    ;;
+esac
+########## Installing dependencies ##########
+
+
+########## Installing the script ##########
+echo "creating docker-compose-manager directory ..."
+mkdir -r $HOME/lib/docker-compose-manager/
+echo "downloading files ..."
+curl -sSL https://raw.githubusercontent.com/naskio/docker-compose-manager/main/docker-compose-manager.$VERSION.sh > $HOME/lib/docker-compose-manager/docker-compose-manager.sh
+echo "adding executable permissions ..."
+chmod +x $HOME/lib/docker-compose-manager/docker-compose-manager.sh
+echo "creating symbolic link ..."
+ln -s $HOME/lib/docker-compose-manager/docker-compose-manager.sh $HOME/bin/dcmanager
+echo "Installation done."
+########## Installing the script ##########
